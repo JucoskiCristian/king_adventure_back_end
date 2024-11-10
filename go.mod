ed8408c3baa58363a19348861f99007d24ed65b4
Cristian Jucoski <cristian.jucoski@gmail.com>
2024-11-10 14:44:39 -0300

    fix alterações de variavel de ambiente correção de rotas

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module kings_adventure
 
 go 1.23.1
 
+require golang.org/x/crypto v0.29.0
+
 require (
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
-	golang.org/x/crypto v0.29.0
+	github.com/rs/cors v1.11.1 // indirect
 )
--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/joho/godotenv"
+	"github.com/rs/cors"
 	"golang.org/x/crypto/bcrypt"
 
 	_ "github.com/lib/pq"
@@ -31,38 +33,56 @@ type Score struct {
 var db *sql.DB
 
 func main() {
-	var err error
+	// Tente carregar o arquivo .env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Erro ao carregar o arquivo .env")
+	}
+
+	// Agora que o arquivo foi carregado, acesse as variáveis
 	connStr := os.Getenv("DATABASE_URL")
 
 	if connStr == "" {
-		log.Fatalf("Variável de ambiente DATABASE_URL não está configurada")
+		log.Fatal("Variável de ambiente DATABASE_URL não está configurada")
 	}
 
+	// Abra a conexão com o banco de dados
 	db, err = sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatalf("Erro ao conectar com o banco de dados: %v", err)
 	}
 	defer db.Close()
 
+	// Verifica se a conexão com o banco foi bem-sucedida
 	if err = db.Ping(); err != nil {
 		log.Fatalf("Erro ao conectar com o banco de dados: %v", err)
 	}
 
+	// Define as rotas
 	http.HandleFunc("/register", registerHandler)
 	http.HandleFunc("/login", loginHandler)
 	http.HandleFunc("/score", addScoreHandler)
 	http.HandleFunc("/scores", getTopScoresHandler)
 	http.HandleFunc("/docs", docsHandler)
 
+	// Configuração do CORS
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"}, // Permite requisições da origem do front-end
+		AllowedMethods: []string{"GET", "POST"}, // Métodos permitidos
+		AllowedHeaders: []string{"Content-Type", "Authorization"}, // Cabeçalhos permitidos
+	})
+
+	// Envolva o servidor HTTP com o middleware CORS
+	handler := c.Handler(http.DefaultServeMux)
+
+	// Inicia o servidor na porta definida no .env ou na porta 8080 por padrão
 	port := os.Getenv("PORT")
 	if port == "" {
-			port = "8080"
+		port = "8080"
 	}
-	
+
 	fmt.Printf("Servidor rodando em http://localhost:%s\n", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-			log.Fatalf("Erro ao iniciar o servidor: %v", err)
-	}
+	log.Fatal(http.ListenAndServe(":"+port, handler))
 }
 
 // Função para registrar um usuário com verificação de duplicidade
@@ -88,12 +108,22 @@ func registerHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if exists {
-		http.Error(w, "Usuário já existe", http.StatusConflict)
+		// Retorna mensagem informando que o usuário já existe
+		w.Header().Set("Content-Type", "application/json")
+		http.Error(w, fmt.Sprintf(`{"error": "Usuário '%s' já existe"}`, user.Username), http.StatusConflict)
 		return
 	}
 
-	// Insere o novo usuário no banco de dados
-	_, err = db.Exec("INSERT INTO users (username, password) VALUES ($1, $2)", user.Username, user.Password)
+	// Criptografa a senha
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		log.Printf("Erro ao criptografar senha: %v", err)
+		http.Error(w, "Erro ao criptografar a senha", http.StatusInternalServerError)
+		return
+	}
+
+	// Insere o novo usuário no banco de dados com a senha criptografada
+	_, err = db.Exec("INSERT INTO users (username, password) VALUES ($1, $2)", user.Username, string(hashedPassword))
 	if err != nil {
 		log.Printf("Erro ao inserir usuário: %v", err)
 		http.Error(w, "Erro ao registrar usuário", http.StatusInternalServerError)
@@ -107,45 +137,44 @@ func registerHandler(w http.ResponseWriter, r *http.Request) {
 // Função para fazer login
 func loginHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-			http.Error(w, "Método não permitido", http.StatusMethodNotAllowed)
-			return
+		http.Error(w, "Método não permitido", http.StatusMethodNotAllowed)
+		return
 	}
 
 	var user User
 	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
-			http.Error(w, "Erro ao decodificar JSON", http.StatusBadRequest)
-			return
+		http.Error(w, "Erro ao decodificar JSON", http.StatusBadRequest)
+		return
 	}
 
 	var userID int
 	var hashedPassword string
 	err := db.QueryRow("SELECT id, password FROM users WHERE username = $1", user.Username).Scan(&userID, &hashedPassword)
 	if err != nil {
-			if err == sql.ErrNoRows {
-					http.Error(w, "Credenciais inválidas", http.StatusUnauthorized)
-			} else {
-					log.Printf("Erro ao consultar banco de dados: %v", err)
-					http.Error(w, "Erro ao acessar o banco de dados", http.StatusInternalServerError)
-			}
-			return
+		if err == sql.ErrNoRows {
+			http.Error(w, "Credenciais inválidas", http.StatusUnauthorized)
+		} else {
+			log.Printf("Erro ao consultar banco de dados: %v", err)
+			http.Error(w, "Erro ao acessar o banco de dados", http.StatusInternalServerError)
+		}
+		return
 	}
 
 	// Compara a senha recebida com o hash armazenado
 	if err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(user.Password)); err != nil {
-			http.Error(w, "Credenciais inválidas", http.StatusUnauthorized)
-			return
+		http.Error(w, "Credenciais inválidas", http.StatusUnauthorized)
+		return
 	}
 
 	// Retorna o ID e o nome do usuário após o login bem-sucedido
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(map[string]interface{}{
-			"message":  "Login bem-sucedido",
-			"user_id":  userID,
-			"username": user.Username,
+		"message":  "Login bem-sucedido",
+		"user_id":  userID,
+		"username": user.Username,
 	})
 }
-
 
 // Função para adicionar um score
 func addScoreHandler(w http.ResponseWriter, r *http.Request) {
@@ -172,6 +201,7 @@ func addScoreHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Score %d registrado com sucesso para o usuário ID %d", score.Score, score.UserID)
 }
 
+// Função para pegar os top scores
 func getTopScoresHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		http.Error(w, "Método não permitido", http.StatusMethodNotAllowed)
@@ -209,154 +239,69 @@ func getTopScoresHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 }
+
 // Função para servir a página de documentação
 func docsHandler(w http.ResponseWriter, r *http.Request) {
-	htmlContent := `
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Documentação da API</title>
-    <style>
-      body {
-        font-family: Arial, sans-serif;
-        margin: 0;
-        padding: 0;
-        background-color: #f7f7f7;
-      }
-      header {
-        background-color: #333;
-        color: #fff;
-        padding: 20px;
-        text-align: center;
-      }
-      .container {
-        padding: 20px;
-        max-width: 800px;
-        margin: 0 auto;
-        background-color: #fff;
-        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-      }
-      h1,h2 {
-        color: #fff;
-				
-      }
-      .endpoint {
-        margin-bottom: 20px;
-      }
-      .endpoint h3 {
-        margin-top: 0;
-      }
-      .method {
-        display: inline-block;
-        padding: 5px 10px;
-        margin-bottom: 10px;
-        font-size: 0.9em;
-        font-weight: bold;
-        color: #fff;
-        border-radius: 5px;
-      }
-      .post {
-        background-color: #4caf50;
-      }
-      .get {
-        background-color: #2196f3;
-      }
-      .description {
-        margin: 10px 0;
-      }
-      pre {
-        background-color: #f4f4f4;
-        padding: 10px;
-        border-radius: 5px;
-        overflow-x: auto;
-      }
-    </style>
-  </head>
-  <body>
-    <header>
-      <h1>Documentação da API</h1>
-      <h2>
-        Bem-vindo à documentação da API. Abaixo estão os detalhes para cada endpoint
-        disponível.
-      </h2>
-    </header>
-    <div class="container">
-      <div class="endpoint">
-        <h3>/register</h3>
-        <span class="method post">POST</span>
-        <p class="description">Registrar novos usuários.</p>
-        <h4>Corpo da Requisição</h4>
-        <pre>
-{
-    "username": "string",
-    "password": "string"
-}</pre>
-        <h4>Resposta</h4>
-        <pre>
-{
-    "message": "Usuário 'NOME_DO_USUÁRIO' registrado com sucesso"
-}</pre>
-      </div>
-
-      <div class="endpoint">
-        <h3>/login</h3>
-        <span class="method post">POST</span>
-        <p class="description">
-          Retorna uma mensagem de sucesso para login, junto com o ID e nome de usuário.
-        </p>
-        <h4>Corpo da Requisição</h4>
-        <pre>
-{
-    "username": "string",
-    "password": "string"
-}</pre>
-        <h4>Resposta</h4>
-        <pre>
-{
-  "message":  "Login bem-sucedido",
-  "user_id":  "integer",
-  "username": "string"
-}</pre>
-      </div>
-
-      <div class="endpoint">
-        <h3>/score</h3>
-        <span class="method post">POST</span>
-        <p class="description">Adicionar uma pontuação para um usuário.</p>
-        <h4>Corpo da Requisição</h4>
-        <pre>
-{
-    "user_id": "integer",
-    "score": "integer"
-}</pre>
-        <h4>Resposta</h4>
-        <pre>
-{
-    "message": "Pontuação 'score' registrada com sucesso para o usuário ID 'user_id'"
-}</pre>
-      </div>
-
-      <div class="endpoint">
-        <h3>/scores</h3>
-        <span class="method get">GET</span>
-        <p class="description">Retorna o top 10 de pontuações em ordem decrescente.</p>
-        <h4>Resposta</h4>
-        <pre>
-[
-    {
-        "user_id": "integer",
-        "username": "string",
-        "score": "integer"
-    },
-    ...
-]</pre>
-      </div>
-    </div>
-  </body>
-</html>
-	`
+	htmlContent := `<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Documentação da API</title>
+		<style>
+			body { font-family: Arial, sans-serif; margin: 0; padding: 0; background-color: #f7f7f7; }
+			header { background-color: #333; color: #fff; padding: 10px 0; text-align: center; }
+			h1 { margin: 0; font-size: 24px; }
+			main { padding: 20px; }
+			section { margin-bottom: 20px; }
+			h2 { color: #333; font-size: 20px; }
+			code { background-color: #f2f2f2; padding: 5px 10px; border-radius: 4px; }
+			ul { margin: 0; padding-left: 20px; }
+		</style>
+	</head>
+	<body>
+		<header>
+			<h1>Documentação da API</h1>
+		</header>
+		<main>
+			<section>
+				<h2>1. Registro de Usuário</h2>
+				<p>Rota: <code>POST /register</code></p>
+				<p>Descrição: Registra um novo usuário.</p>
+				<p>Parâmetros:</p>
+				<ul>
+					<li><code>username</code>: Nome de usuário.</li>
+					<li><code>password</code>: Senha do usuário.</li>
+				</ul>
+			</section>
+			<section>
+				<h2>2. Login de Usuário</h2>
+				<p>Rota: <code>POST /login</code></p>
+				<p>Descrição: Faz login de um usuário.</p>
+				<p>Parâmetros:</p>
+				<ul>
+					<li><code>username</code>: Nome de usuário.</li>
+					<li><code>password</code>: Senha do usuário.</li>
+				</ul>
+			</section>
+			<section>
+				<h2>3. Adicionar Score</h2>
+				<p>Rota: <code>POST /score</code></p>
+				<p>Descrição: Adiciona um score para um usuário.</p>
+				<p>Parâmetros:</p>
+				<ul>
+					<li><code>user_id</code>: ID do usuário.</li>
+					<li><code>score</code>: Valor do score.</li>
+				</ul>
+			</section>
+			<section>
+				<h2>4. Top Scores</h2>
+				<p>Rota: <code>GET /scores</code></p>
+				<p>Descrição: Retorna os top 10 scores.</p>
+			</section>
+		</main>
+	</body>
+	</html>`
 	w.Header().Set("Content-Type", "text/html")
 	w.Write([]byte(htmlContent))
 }
